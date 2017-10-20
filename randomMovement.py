4fc70bf12eb0dc361f5dc45de62c8d592d824d4c
dyth <dythui2@gmail.com>
2017-10-20 18:55:11 +0100

    giraffe starting point

--- randomMovement.py
+++ randomMovement.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
+
+import fileinput, sys
 """
 agent which moves randomly 
+
+
+
+with open('move.txt', 'wab') as f:
+    while True:
+        #fi = fileinput.input()
+        for line in fileinput.input():
+            f.write(line)
+        #fi += 'end'
+        #f.write(fi)
+        #f.write('alone')
+        print "e7e6\n"
 """
-
-
-while True:
-    print input()
-    
+while True
+    fileinput.input()
+    sys.stdout.write("e7e6\n")
+    sys.stdout.stream.flush()
